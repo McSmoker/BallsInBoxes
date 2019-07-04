bda97d16a82a825d1531b1efb7e89e294756ae40
Anne _ <anne1993@live.nl>
2019-07-05 00:06:30 +0200

    InitialCommit

---
+++ Assets/Scripts/BuildingGridManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingGridManager : MonoBehaviour
+{
+    Vector3[,] buildingGrid = new Vector3[22, 22];
+    Vector3 startPos = new Vector3(-104, 0,-104);
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < 22; i++)
+        {
+            for (int i2 = 0; i2 < 22; i2++)
+            {
+                buildingGrid[i, i2] = startPos;
+                startPos += new Vector3(i2,0,0);
+            }
+            startPos += new Vector3(0, 0, i);
+        }
+        Debug.Log("wuuuuut");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/BuildingManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingManager : MonoBehaviour
+{
+    //pointless
+    [SerializeField]
+    CameraMovement playerCamera;
+    //niet pointless
+    [SerializeField]
+    SquarePlatform squarePlatformClass;
+
+    Vector3 point,point2;
+    Vector3 hitPosition;
+
+    SquarePlatform platformToBePlaced;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerCamera = GameState.Instance.PlayerCamera;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        
+        if (Input.GetMouseButtonDown(0)) //check if the LMB is clicked
+        {
+            RaycastHit hit;
+            Ray ray = playerCamera.GetComponent<Camera>().ScreenPointToRay(Input.mousePosition);
+        
+            if (Physics.Raycast(ray, out hit)) //check if the ray hit something
+            {
+                hitPosition = hit.point; //use this position for what you want to do
+            }
+        Debug.Log(hitPosition);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Q))
+             spawnPlatform();
+        if (platformToBePlaced != null)
+            platformToBePlaced.transform.position = hitPosition;
+        point = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        point2 = Camera.main.transform.position;
+        
+    }
+
+    void spawnPlatform()
+    {
+        platformToBePlaced = Instantiate(squarePlatformClass, point,new Quaternion(0,0,0,0));
+        Instantiate(squarePlatformClass, point, new Quaternion(0, 0, 0, 0));
+    }
+}
