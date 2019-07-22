0e0eece0b2ff701dfd007b6206ac35b84f839a10
Anne _ <anne1993@live.nl>
2019-07-22 23:23:55 +0200

    Vergete vorige unversioned te comitten-.- zelfde message

---
+++ BallsInBoxes/Assets/Scripts/Data/DataMissions.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Mission", menuName = "Mission Data", order = 51)]
+public class MissionData : ScriptableObject
+{
+    [SerializeField]
+    private string name;
+    [SerializeField]
+    private string objective;
+    [SerializeField]
+    private string reward;
+    /// <summary>
+    /// /??????ik weet niet maar whatuppppp
+    /// </summary>
+    public bool Completed { get; }
+
+    public string Name
+    {
+        get
+        {
+            return name;
+        }
+    }
+
+    public string Objective
+    {
+        get
+        {
+            return objective;
+        }
+    }
+
+    public string Reward
+    {
+        get
+        {
+            return reward;
+        }
+    }
+}
